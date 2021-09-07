f20ad76f8d3f2b7dfe73aa28f5cd1993d76888a9
Skyler Rivera <skylerrivera17@yahoo.com>
2021-09-07 17:19:54 -0400

    first pass at adding validation to User model

--- models/user.js
+++ models/user.js
@@ -20,10 +20,61 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    emailAddress: DataTypes.STRING,
-    password: DataTypes.STRING
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A first name is required.'
+        },
+        notEmpty: {
+          msg: 'Please provide a first name.'
+        }
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A last name is required.'
+        },
+        notEmpty: {
+          msg: 'Please provide a last name.'
+        }
+      }
+    },
+    emailAddress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        msg: 'The email you entered already exists.'
+      },
+      validate: {
+        notNull: {
+          msg: 'A email address is required.'
+        },
+        isEmail: {
+          msg: 'Please provide a valid email address.'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A password is required.'
+        },
+        notEmpty: {
+          msg: 'Please provide a password.'
+        },
+        len: {
+          args: [8, 20],
+          msg: 'The password should be between 8 and 20 characters in length.'
+        }
+      }
+    }
   }, {
     sequelize,
     modelName: 'User',
